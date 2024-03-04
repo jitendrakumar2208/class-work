374b9ba5609328b87799bf97618670894280a58e
jitendrakumar2208 <erjitendrakumar2208@gmail.com>
2024-03-05 00:15:55 +0530

    done

---
+++ app.js
@@ -0,0 +1,18 @@
+var conn = (
+    <div class = "box">
+     <h1 class = "box">Learn Web Development</h1>
+     <br />
+     <p>Welcome to the MDN Learning area. This set of articles aims to guide complete to web development with all that they need to start conding website</p>
+
+     <br />
+
+     <p >The aim of this area of MDN is not to take you from "beginner" to "expert" but to take you from "beginner" to "comfortable." From there, you should be able to start making your way, learning from the rest of MDN, and other intermediate to advanced resources that assume a lot of previous knowledge. </p>
+        
+    <br/>
+
+     <p>If you are a complete beginner, web development can be challenging — we will hold your hand and provide enough detail for you to feel comfortable and learn the topics properly. You should feel at home whether you are a student learning web development (on your own or as part of a class), a teacher looking for class
+materials, a hobbyist, or someone who just wants to understand more about how web technologies work. </p>
+    </div>
+)
+
+ReactDOM.render(conn,document.getElementById("root"));
